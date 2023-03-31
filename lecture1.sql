26fe5d5031556eea06f467cb51a8ae092da58aec
asilbekpulatoff <asilbekpolatov77@gmail.com>
2023-03-31 21:03:53 +0500

    ex1

---
+++ lecture1.sql
@@ -0,0 +1,8 @@
+--entertainmentagencyexample_hw
+select agtfirstname, agtlastname, agentID from agents order by agtfirstname DESC ;
+select * from engagements;
+select All startdate from engagements order by startdate AS;
+
+--recipesexample_hw
+select ingredientclassID, ingredientname from ingredients;
+select recipetitle from recipes  ORDER BY recipetitle ASC;
