845c3e417de66895f228cdfce09a63b9fd083b03
asilbekpulatoff <asilbekpolatov77@gmail.com>
2023-04-13 13:59:51 +0500

    full works

--- quiz1.sql
+++ quiz1.sql
@@ -1,7 +1,12 @@
-select count(recipeclassdescription) from recipe_classes 
- where recipeclassdescription = 'Main course'
-
-select recipetitle from recipes where recipetitle not like notes
-
+1
 select  distinct vendname, count(productnumber) from product_vendors inner join vendors using(vendorid)
 group by productnumber, vendname
+2
+select count(recipeclassdescription)from recipe_classes 
+ where recipeclassdescription = 'Main course' group by recipeclassdescription
+3
+select teamname, bowlerlastname from bowlers inner join teams using (teamid) where teamid in (3, 4, 5) 
+and bowlerlastname like 'H%'
+4
+select recipetitle from recipes where notes is null
+group by recipetitle
